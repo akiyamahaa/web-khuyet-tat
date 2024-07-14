7aab22959c654f1a2667e7f744a6c8287fb49693
Quang Akiyama <quanganhcf2@gmail.com>
2024-07-14 17:31:37 +0700

    remove warning

--- src/routes/GuestRoute.tsx
+++ src/routes/GuestRoute.tsx
@@ -3,10 +3,11 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 type Props = {};
 
+// eslint-disable-next-line no-empty-pattern
 const GuestRoute = ({}: Props) => {
   const location = useLocation();
   // const { user } = useAppSelector((state: RootState) => state.user);
-  const user = true
+  const user = true;
   return user ? (
     <Navigate to={"/"} state={{ from: location }} replace />
   ) : (
