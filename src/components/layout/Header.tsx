6cca519b5ec0d32e00b024f89116c2ca77d15e2f
vanh1604 <VietAnhScout@gmail.com>
2024-07-21 17:20:54 +0700

    responsive header and footer

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,68 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {
-  ABOUT_PATH,
-  ROOT,
-  INTRODUCTION_PATH,
-  ENVENT_PATH,
-  BLOG_PATH,
-  STORE_PATH,
-} from "@/routes/constant";
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+// import "./Header.css";
 import { CiSearch } from "react-icons/ci";
 
 function Header() {
+  const location = useLocation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  // const dropdownRef = useRef(null);
+  const isActive = (path: string) => location.pathname === path;
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => {
+  //     document.removeEventListener("mousedown", handleClickOutside);
+  //   };
+  // }, []);
   return (
     <div className="flex justify-center h-16">
-      <div className="container flex justify-between  items-center">
+      <div className="container flex justify-between  items-center md:px-5">
+        <div className="py-2 relative block md:hidden">
+          <button onClick={toggleDropdown} className="dropdown-toggle">
+            More
+          </button>
+          {dropdownOpen && (
+            <ul className="dropdown-menu absolute bg-white shadow-lg mt-2 py-2 w-48">
+              <li className={`py-2 ${isActive("/") ? "text-blue-400" : ""}`}>
+                <NavLink to="/">Trang Chủ</NavLink>
+              </li>
+              <li
+                className={`py-2 ${
+                  isActive("/introduction") ? "text-blue-400" : ""
+                }`}
+              >
+                <NavLink to="/introduction">Giới thiệu</NavLink>
+              </li>
+              <li
+                className={`py-2 ${isActive("/events") ? "text-blue-400" : ""}`}
+              >
+                <NavLink to="/events">Sự kiện</NavLink>
+              </li>
+              <li
+                className={`py-2 ${
+                  isActive("/products") ? "text-blue-400" : ""
+                }`}
+              >
+                <NavLink to="/products">Sản phẩm</NavLink>
+              </li>
+              <li
+                className={`py-2 ${isActive("/blog") ? "text-blue-400" : ""}`}
+              >
+                <NavLink to="/blog">Bài viết</NavLink>
+              </li>
+              <li
+                className={`py-2 ${isActive("/about") ? "text-blue-400" : ""}`}
+              >
+                <NavLink to="/about">Liên hệ</NavLink>
+              </li>
+            </ul>
+          )}
+        </div>
         <div>
           <img
             src="https://reactjs.org/logo-og.png"
@@ -21,33 +70,42 @@ function Header() {
             className="w-12 object-cover"
           />
         </div>
-        <ul className="flex gap-4">
-          <li className="py-2">
-            <Link to={ROOT}>Trang Chủ</Link>
-          </li>
-          <li className="py-2">
-            {" "}
-            <Link to={INTRODUCTION_PATH}>Giới thiệu</Link>
-          </li>
-          <li className="py-2">
-            {" "}
-            <Link to={ENVENT_PATH}>Sự kiện</Link>
-          </li>
-          <li className="py-2">
-            {" "}
-            <Link to={STORE_PATH}>Cửa Hàng</Link>
-          </li>
-          <li className="py-2">
-            {" "}
-            <Link to={BLOG_PATH}>Bài viết</Link>
-          </li>
-          <li className="py-2">
-            <Link to={ABOUT_PATH}>Liên hệ</Link>
-          </li>
-          <li className="py-2 ">
-            <CiSearch className="text-[20px]" />
-          </li>
-        </ul>
+        <nav className="navbar flex max-md:hidden">
+          <ul className="gap-4 hidden md:flex">
+            <li className={`py-2 ${isActive("/") ? "text-blue-400" : ""}`}>
+              <NavLink to="/">Trang Chủ</NavLink>
+            </li>
+            <li
+              className={`py-2 ${
+                isActive("/introduction") ? "text-blue-400" : ""
+              }`}
+            >
+              <NavLink to="/introduction">Giới thiệu</NavLink>
+            </li>
+            <li
+              className={`py-2 ${isActive("/events") ? "text-blue-400" : ""}`}
+            >
+              <NavLink to="/events">Sự kiện</NavLink>
+            </li>
+            <li
+              className={`py-2 ${isActive("/products") ? "text-blue-400" : ""}`}
+            >
+              <NavLink to="/products">Sản phẩm</NavLink>
+            </li>
+            <li className={`py-2 ${isActive("/blog") ? "text-blue-400" : ""}`}>
+              <NavLink to="/blog">Bài viết</NavLink>
+            </li>
+            <li className={`py-2 ${isActive("/about") ? "text-blue-400" : ""}`}>
+              <NavLink to="/about">Liên hệ</NavLink>
+            </li>
+            <li className="py-2 ">
+              <CiSearch className="text-[20px]" />
+            </li>
+          </ul>
+        </nav>
+        <div className="py-2 block md:hidden">
+          <CiSearch className="text-[20px]" />
+        </div>
       </div>
     </div>
   );
