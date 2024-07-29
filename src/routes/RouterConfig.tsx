b6e590cb62e3ce5440a3b5756a8add9456cdd739
vanh1604 <VietAnhScout@gmail.com>
2024-07-29 16:49:46 +0700

    finish ui

--- src/routes/RouterConfig.tsx
+++ src/routes/RouterConfig.tsx
@@ -7,43 +7,57 @@ import NotFound from "@/pages/error/NotFound";
 import GuestRoute from "./GuestRoute";
 import Login from "@/pages/auth/Login";
 import Signup from "@/pages/auth/Signup";
-
+import { INTRODUCTION_PATH } from "./constant";
+import Introduction from "@/pages/Introduction";
 
 type Props = {};
 
 export const ROLES = {
-  USER: "0",
-  ADMIN: "1",
+	USER: "0",
+	ADMIN: "1",
 };
 
 const RouterConfig = (props: Props) => {
+	return (
+		<Routes>
+			{/* Add error, loading */}
 
-  return (
-    <Routes>
-      {/* Add error, loading */}
-
-      {/* public routes */}
-      <Route element={<LayoutRoute />}>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        {/* For user */}
-        <Route element={<ProtectedRoute allowedRoles={[ROLES.USER]} />}>
-          {/* <Route path="/profile/:id" element={<Profile />} /> */}
-        </Route>
-        {/* For Admin */}
-        <Route element={<ProtectedRoute allowedRoles={[ROLES.ADMIN]} />}>
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        </Route>
-        {/* Catch all */}
-        <Route path="*" element={<NotFound />} />
-      </Route>
-
-      <Route element={<GuestRoute />}>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Route>
-    </Routes>
-  );
+			{/* public routes */}
+			<Route element={<LayoutRoute />}>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="about"
+					element={<About />}
+				/>
+				<Route
+					path={INTRODUCTION_PATH}
+					element={<Introduction />}
+				/>
+				{/* For user */}
+				<Route element={<ProtectedRoute allowedRoles={[ROLES.USER]} />}>{/* <Route path="/profile/:id" element={<Profile />} /> */}</Route>
+				{/* For Admin */}
+				<Route element={<ProtectedRoute allowedRoles={[ROLES.ADMIN]} />}>{/* <Route path="/dashboard" element={<Dashboard />} /> */}</Route>
+				{/* Catch all */}
+				<Route
+					path="*"
+					element={<NotFound />}
+				/>
+			</Route>
+			<Route element={<GuestRoute />}>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/signup"
+					element={<Signup />}
+				/>
+			</Route>
+		</Routes>
+	);
 };
 
 export default RouterConfig;
